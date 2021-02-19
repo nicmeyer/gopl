6fd5e52c6781534cc4c5d74b8deaedb9a55f2ae2
nicmeyer <nicholasmeyer92@gmail.com>
2021-02-19 09:27:20 +0200

    Commit progress on chapter 3.

--- ch3/ex7/mandelbrot.go
+++ ch3/ex7/mandelbrot.go
@@ -16,32 +16,14 @@ func main() {
 		width, height          = 1024, 1024
 	)
 
-	var imgL [2*width][2*height]color.Color
-	for py := 0; py < height*2; py++ {
-		y := float64(py)/(height*2)*(ymax-ymin) + ymin
-		for px := 0; px < width*2; px++ {
-			x := float64(px)/(width*2)*(xmax-xmin) + xmin
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
 			z := complex(x, y)
 			// Image point (px, py) represents complex value z.
-			imgL[px][py] = mandelbrot(z)
-		}
-	}
-	imgH := image.NewRGBA(image.Rect(0, 0, width, height))
-	for py := 0; py < height*2; py += 2 {
-		for px := 0; px < width*2; px += 2 {
-			r1, g1, b1, a1 := imgL[px][py].RGBA()
-			r2, g2, b2, a2 := imgL[px+1][py].RGBA()
-			r3, g3, b3, a3 := imgL[px][py+1].RGBA()
-			r4, g4, b4, a4 := imgL[px+1][py+1].RGBA()
-
-			R := r1 + r2 + r3 + r4
-			G := g1 + g2 + g3 + g4
-			B := b1 + b2 + b3 + b4
-			A := a1 + a2 + a3 + a4
-
-			imgH.Set(px/2, py/2, color.RGBA{
-				uint8(R/1028), uint8(G/1028), uint8(B/1028), uint8(A/1028),
-			})
+			img.Set(px, py, mandelbrot(z))
 		}
 	}
 	f, err := os.Create("mandelbrot.png")
@@ -49,7 +31,7 @@ func main() {
 		fmt.Fprintf(os.Stdout, "mandelbrot: %v\n", err)
 		os.Exit(1)
 	}
-	if err := png.Encode(f, imgH); err != nil {
+	if err := png.Encode(f, img); err != nil {
 		f.Close()
 		fmt.Fprintf(os.Stdout, "mandelbrot: %v\n", err)
 		os.Exit(1)
